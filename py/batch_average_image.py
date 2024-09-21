878b86ea5fb70c83886f302ab02d20e1b94f0c17
ruucm <ruucm.a@gmail.com>
2024-09-21 12:59:46 +0900

    BatchAverageImage (from ComfyUI-Image-Filters)

---
+++ py/batch_average_image.py
@@ -0,0 +1,32 @@
+import torch
+
+class BatchAverageImage:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "images": ("IMAGE",),
+                "operation": (["mean", "median"],),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "apply"
+
+    CATEGORY = "image/filters"
+
+    def apply(self, images, operation):
+        t = images.detach().clone()
+        if operation == "mean":
+            return (torch.mean(t, dim=0, keepdim=True),)
+        elif operation == "median":
+            return (torch.median(t, dim=0, keepdim=True)[0],)
+        return (t,)
+
+NODE_CLASS_MAPPINGS = {
+    "BatchAverageImage": BatchAverageImage,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "BatchAverageImage": "Batch Average Image",
+}
