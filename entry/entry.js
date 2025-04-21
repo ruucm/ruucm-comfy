4c0cbe2f1ee5a18f755a2326c3b8444dfb8a0142
ruucm <ruucm.a@gmail.com>
2025-04-21 20:19:59 +0900

    disable /ruucm_comfy_web/input import

--- entry/entry.js
+++ entry/entry.js
@@ -2,6 +2,6 @@
 import { api } from "../../scripts/api.js";
 
 setTimeout(() => {
-  console.log("importing ruucm_comfy_web/input.js");
-  import(api.api_base + "/ruucm_comfy_web/input.js");
+  // console.log("importing ruucm_comfy_web/input.js");
+  // import(api.api_base + "/ruucm_comfy_web/input.js");
 }, 500);
